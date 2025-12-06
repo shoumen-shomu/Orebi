006cd69ac7e75b7dca4361143599c127ee0de4c1
Shoumen <bshoumen@gmail.com>
2025-12-07 03:28:46 +0600

    add routing and layout components; update dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-
-import './App.css'
+import RootLayout from "./assets/components/layouts/RootLayout";
+import { Routes, Route } from "react-router-dom";
+import Home from "./assets/components/pages/Home";
+import Shop from "./assets/components/pages/Shop";
+import Error from "./assets/components/pages/Error";
+import "./App.css";
 
 function App() {
-  
-
   return (
     <>
-    <h1>Hello World</h1>
+      <Routes>
+        <Route path="/" element={<RootLayout />}>
+          <Route index element={<Home />} />
+          <Route path="shop" element={<Shop />} />
+          <Route path="*" element={<Error />} />
+        </Route>
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
